36b713fa62388f01a18d4ba4aab59cedfb8ae0a2
Diemerson <diemerson.silva@live.com>
2023-10-29 21:30:58 -0300

    desafio concluido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,18 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes == null)
+            {
+                throw new ArgumentNullException(nameof(hospedes), "A lista de hóspedes não pode ser nula.");
+            }
+
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new ArgumentException($"Capacidade insuficiente. A suíte comporta no máximo {Suite.Capacidade} hóspede(s), mas foram recebidos {hospedes.Count}.");
             }
         }
 
@@ -35,23 +37,18 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            const decimal taxaDesconto = 0.10m;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal valorDesconto = valor * taxaDesconto;
+                valor = valor - valorDesconto;
             }
 
             return valor;
